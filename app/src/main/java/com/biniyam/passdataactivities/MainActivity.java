81051d2dc7a1a3fc2d85b0130d481dd7301d658c
bygxs <114972065+bygxs@users.noreply.github.com>
2023-01-16 00:07:36 +0100

    00:05 Mon 16 Jan 2023 First commit  created serializable single object using  intent.putExtra(USER_KEY, new User("",22));

---
+++ app/src/main/java/com/biniyam/passdataactivities/MainActivity.java
@@ -0,0 +1,47 @@
+package com.biniyam.passdataactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity {
+    String TAG;
+    String NAME_KEY = "NAME";
+    final static public String USER_KEY = "USER";
+
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate");
+
+        setTitle("Main Activity");
+
+        button = findViewById(R.id.button);
+                button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra(NAME_KEY,"Bob Smith");
+                // a serializable method of an object
+                intent.putExtra(USER_KEY, new User("",22));
+                startActivity(intent);
+
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/biniyam/passdataactivities/User.java
@@ -0,0 +1,16 @@
+package com.biniyam.passdataactivities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    String name;
+    int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+}
